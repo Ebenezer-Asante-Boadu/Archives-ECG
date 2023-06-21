a021b3eb0c35484b4fd085979b28dbbb61176c51
DERRICK-TORKORNOO <116164431+DERRICK-TORKORNOO@users.noreply.github.com>
2023-06-21 18:09:44 +0000

    HOMEPAGE

--- front-end/src/views/homepage/homepage.ts
+++ front-end/src/views/homepage/homepage.ts
@@ -42,8 +42,13 @@ class Home {
     private groupCard(){
         let div = document.createElement("div");
         div.classList.add("group-card");
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
+        div.appendChild(grid);
 
         function card(){
+            let elements:any = [];
+            
             const cards: {
                 [key: string]: {
                     img: string;
@@ -57,7 +62,7 @@ class Home {
                     writing: "Efficiency in our archives is a multifaceted endeavor, encompassing organization, accessibility, preservation, technology integration, workflow optimization, and continuous improvement. By striving for excellence in our area, archives can achieve higher levels of efficiency and effectively serve their intended purposes."
                 },
                 "card2": {
-                    img: "/asstes/affordable.png", 
+                    img: "/assets/affordable.png", 
                     title: "AFFORDABLE", 
                     writing: "While achieving absolute affordability for an archive may be challenging, implementing cost-effective strategies, leveraging partnerships, and making informed decisions regarding resource allocation can contribute to the financial sustainability and accessibility of an archive while ensuring its core objectives are met."
                 },
@@ -69,23 +74,80 @@ class Home {
             }
 
             for(let i = 0; i < Object.keys(cards).length; i++){
-                
                 let element = document.createElement("div");
+                
                 element.classList.add(Object.keys(cards)[i]);
 
                 function image(){
+                    let wrapper = document.createElement("div");
+                    wrapper.classList.add("image-wrapper");
                     let img =document.createElement("img");
                     let key = Object.keys(cards)[i]
                     img.setAttribute("src", cards[key].img);
                     img.setAttribute("alt", `${cards[key].title} image`);
-                    return img;
+                    wrapper.appendChild(img);
+                    return wrapper;
                 }
+
+                function title(){
+                    let element = document.createElement("p");
+                    let txt = document.createTextNode(cards[Object.keys(cards)[i]].title);
+                    element.classList.add("title");
+                    element.appendChild(txt);
+                    return element;
+                }
+
+                function writing(){
+                    let element = document.createElement("p");
+                    element.classList.add("writing");
+                    let txt = document.createTextNode(cards[Object.keys(cards)[i]].writing);
+                    element.appendChild(txt);
+                    return element;
+                }
+                element.append(image(), title(), writing());
+                elements.push(element);
             }
+            
+            return elements;
         }
+
+        card().forEach((data:HTMLElement)=>{
+            grid.appendChild(data);
+        });
+        
+        return div;
     }
     
+
+    private middlePic(){
+        let element = document.createElement("div");
+        element.classList.add("middle-pic");
+        return element;
+    }
+
+
+    private middleHeader(){
+        let div = document.createElement("div");
+        div.classList.add("middle-header");
+
+        function title(){
+            let tit = document.createElement("p");
+            tit.classList.add("big-title");
+            let txt = document.createTextNode("")
+            return tit;
+        }
+
+        function caption(){
+            let cap = document.createElement("p");
+            cap.classList.add("caption");
+            return cap;
+        }
+        div.append(title(), caption());
+        return div;
+    }
+
     public assembler() {
-        this.parent.appendChild(this.backgroundFrame());
+        this.parent.append(this.backgroundFrame(), this.groupCard(), this.middlePic(), this.middleHeader());
     }
 }
 
