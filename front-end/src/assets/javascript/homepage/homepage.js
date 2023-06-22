b6c8eb313e633c1e21941e7d231ea55c50d0eb14
DERRICK-TORKORNOO <116164431+DERRICK-TORKORNOO@users.noreply.github.com>
2023-06-22 19:45:50 +0000

    home

--- front-end/src/assets/javascript/homepage/homepage.js
+++ front-end/src/assets/javascript/homepage/homepage.js
@@ -119,8 +119,124 @@ class Home {
         div.append(title(), caption());
         return div;
     }
+    team() {
+        let container = document.createElement("div");
+        container.classList.add("profiles");
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
+        container.appendChild(grid);
+        function cards() {
+            let elements = [];
+            const cards = {
+                "person1": {
+                    profile: "/assets/person1.png",
+                    name: "Kofi Abrefa Gyan",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. ",
+                },
+                "person2": {
+                    profile: "/assets/person2.png",
+                    name: "Kofi Abrefa Gyan",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. ",
+                },
+                "person3": {
+                    profile: "/assets/person3.png",
+                    name: "Kofi Abrefa Gyan",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo in vitae turpis massa sed. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. ",
+                }
+            };
+            for (let i = 0; i < Object.keys(cards).length; i++) {
+                let cover = document.createElement("div");
+                cover.classList.add("cover");
+                function profile() {
+                    let imgContainer = document.createElement("div");
+                    imgContainer.classList.add("img-container");
+                    let img = document.createElement("img");
+                    img.setAttribute("src", cards[Object.keys(cards)[i]].profile);
+                    img.setAttribute("alt", "profile pic");
+                    img.classList.add('profile');
+                    imgContainer.appendChild(img);
+                    return imgContainer;
+                }
+                function name() {
+                    let nm = document.createElement("p");
+                    let txt = document.createTextNode(cards[Object.keys(cards)[0]].name);
+                    nm.appendChild(txt);
+                    return nm;
+                }
+                function description() {
+                    let desc = document.createElement("div");
+                    desc.classList.add("description");
+                    let txt = document.createTextNode(cards[Object.keys(cards)[i]].description);
+                    desc.appendChild(txt);
+                    return desc;
+                }
+                cover.append(profile(), name(), description());
+                elements.push(cover);
+            }
+            return elements;
+        }
+        cards().forEach((data) => {
+            grid.append(data);
+        });
+        function middleLine() {
+            let img = document.createElement("img");
+            img.setAttribute("src", "/assets/horizontal-line.png");
+            img.classList.add("middleLine");
+            return img;
+        }
+        function footer() {
+            let elements2 = [];
+            const foots = {
+                person1: {
+                    iconPosition: "/assets/position.png",
+                    job_title: "Senior Administrative Assistant"
+                },
+                person2: {
+                    iconPosition: "/assets/position.png",
+                    job_title: "Senior Administrative Assistant"
+                },
+                person3: {
+                    iconPosition: "/assets/position.png",
+                    job_title: "Senior Administrative Assistant"
+                },
+            };
+            for (let i = 0; i < Object.keys(foots).length; i++) {
+                let div = document.createElement("div");
+                div.classList.add("card-footer");
+                function image() {
+                    let iconPositionContainer = document.createElement("div");
+                    iconPositionContainer.classList.add("iconPositionContainer");
+                    let img = document.createElement("img");
+                    img.setAttribute("src", foots[Object.keys(foots)[i]].iconPosition);
+                    img.classList.add("iconPosition");
+                    iconPositionContainer.appendChild(img);
+                    return iconPositionContainer;
+                }
+                function jobTitle() {
+                    let para = document.createElement("p");
+                    para.classList.add("job-title");
+                    let txt = document.createTextNode(foots[Object.keys(foots)[i]].job_title);
+                    para.appendChild(txt);
+                    return para;
+                }
+                div.append(image(), jobTitle());
+                elements2.push(div);
+            }
+            return elements2;
+        }
+        grid.appendChild(middleLine());
+        footer().forEach((data) => {
+            grid.appendChild(data);
+        });
+        return container;
+    }
+    footer() {
+        let footerDiv = document.createElement("div");
+        footerDiv.classList.add("footerDiv");
+        return footerDiv;
+    }
     assembler() {
-        this.parent.append(this.backgroundFrame(), this.groupCard(), this.middlePic(), this.middleHeader());
+        this.parent.append(this.backgroundFrame(), this.groupCard(), this.middlePic(), this.middleHeader(), this.team(), this.footer());
     }
 }
 window.addEventListener("load", () => {
